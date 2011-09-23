6a23a407e8fe5e38c85517105292e19195c0975b
Evan <emachnic@engineyard.com>
2011-09-23 15:06:04 -0400

    Bumped Rails to 3.0.10 and added unit tests

--- test/unit/list_test.rb
+++ test/unit/list_test.rb
@@ -2,7 +2,13 @@ require 'test_helper'
 
 class ListTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should create a list given valid attributes" do
+    list = List.new(:name => 'First List')
+    assert(list.save!)
+  end
+  
+  test "should reject a list without a name" do
+    list = List.new(:name => "")
+    assert(!list.save, "Must have a name")
   end
 end
--- test/unit/task_test.rb
+++ test/unit/task_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should create a task given valid attributes" do
+    task = Task.new(:name => 'First Task')
+    assert(task.save!)
+  end
+  
+  test "should reject tasks without names" do
+    task = Task.new(:name => "")
+    assert(!task.save, "Must have a name")
   end
 end
